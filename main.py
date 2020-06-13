d6fd6fdf58a3561e7fa8548cc8954f1b7b5dc4e0
Robert Smith <rsmith5901@gmail.com>
2020-06-12 22:10:53 -0400

    dssdfsfd

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
-from umqtt.simple import MQTTClient
-import time, network
+import time
+from connection import connect
 from machine import Pin
 
-aws_endpoint = 'a24e0i570zhegg.iot.us-east-2.amazonaws.com'
-
-ssl_params = {'keyfile': "/flash/cert/aws.key",
-              'certfile': "/flash/cert/aws.crt",
-              'ca_certs': "/flash/cert/aws.ca"}
 msgs_received = 0
-conn = network.Cellular()
 
 dio10 = Pin("P0", Pin.OUT, value=0)
 
-while not conn.isconnected():
-    print("waiting for network connection...")
-    time.sleep(4)
 print("network connected")
+
+client_id = "oasidfjoasidfjasdf"
+c = connect(client_id)
+
 
 def sub_cb(topic, msg):
     global msgs_received
@@ -23,28 +18,25 @@ def sub_cb(topic, msg):
     print(topic, msg)
     dio10.toggle()
 
-def publish_test(client_id="clientId", ssl_p=ssl_params):
-    # "clientId" should be unique for each device connected
-    c = MQTTClient(client_id, aws_endpoint, ssl=True, ssl_params=ssl_p)
-    print("connecting...")
-    c.connect()
-    print("connected")
+
+def subscribe():
     c.set_callback(sub_cb)
     c.subscribe("sample/xbee")
-    # topic: "sample/xbee"
-    # message: {message: AWS Samples are cool!}
-    print("publishing message...")
-    c.publish("sample/xbee", '{"message": "Yay from Robert\'s Xbee"}')
-    print("published")
 
     global msgs_received
+
     msgs_received = 0
     while msgs_received < 5:
         c.check_msg()
         time.sleep(5)
-
     time.sleep(1)
     c.disconnect()
     print("DONE")
 
+
+def publish_test():
+    c.publish("sample/xbee", '{"message": "Yay from Robert\'s Xbee"}')
+
+
 publish_test()
+subscribe()
